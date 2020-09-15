0a36942b8437d5f088c7a34f3a9524c1be190ce6
Acute-Kr <jkds878@naver.com>
2020-09-15 19:37:15 +0900

    0915

---
+++ 0915/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int sum=0, i=1;
+		while (true) {
+			if(    i==51     )  break;
+			sum+=i;
+			i++;
+		}
+		System.out.println(sum);
+	}
+
+}
---
+++ 0915/src/Main2.java
@@ -0,0 +1,28 @@
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		boolean bool[] = {true,false,false,true};
+		
+		System.out.println(bool[3]);
+		
+		double d[][] = new double[5][4];
+		
+		int val[][] = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+		
+		System.out.println(val[1].length);
+		
+		char [] alpha = {'a','b','c', 'd'};
+		for(int i=0;i<alpha.length;i++) {
+			System.out.println(alpha[i]);
+		}
+		for(char c : alpha) {
+			System.out.println(c);
+		}
+		
+		double p[][] = new double[4][];
+		i[]
+		
+	}
+
+}
